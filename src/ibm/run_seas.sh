3cb9d524b53d20f6d22ac288924a75413a0bbbaa
Bram Kuijper <bramkuijper@gmail.com>
2016-05-13 22:28:39 +0100

    evolving variance and mean of seasonal start, works like a charm

--- src/ibm/run_seas.sh
+++ src/ibm/run_seas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./xseasonality_date 0.02 0.0 0.01     0.05 0.05 0.3 2  1.0 1.0     2.0  0.0 1.0
+./xseasonality_date 0.02 0.0 0.01     0.05 0.05 0.05         2  1.0 2.0     2.0  0.0 1.0
 
 #    mu_a_tau = atof(argv[1]);
 #    mu_a_eps = atof(argv[2]);
@@ -9,9 +9,8 @@
 #    m_n = atof(argv[5]);
 #    m_severe = atof(argv[6]);
 #    b = atoi(argv[7]);
-#    brec = atoi(argv[8]);
-#    clutch_multiplier = atof(argv[9]);
-#    min_clutch = atof(argv[10]);
-#    ampl = atof(argv[11]);
-#    stoch = atof(argv[12]);
-#    sd_envt = atof(argv[13]);
+#    clutch_multiplier = atof(argv[8]);
+#    min_clutch = atof(argv[9]);
+#    ampl = atof(argv[10]);
+#    stoch = atof(argv[11]);
+#    sd_envt = atof(argv[12]);
